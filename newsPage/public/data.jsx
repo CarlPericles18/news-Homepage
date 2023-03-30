3f21ef34805bd6fc25218ac57649d6116106a414
Perc <carlpericles18@gmail.com>
2023-03-31 01:17:18 +0900

    Updated Img

--- newsPage/public/data.jsx
+++ newsPage/public/data.jsx
@@ -5,21 +5,21 @@ export default[
                     number: 1,
                     title:"Reviving Retro Pcs",
                     desc:"What happens when old PCs are giving modern upgrades?",
-                    img: "../public/images/image-retro-pcs.jpg"
+                    img: "./images/image-retro-pcs.jpg"
             },
             {
                     id: 2,
                     number: 2,
                     title: 'Top 10 Laptops of 2022',
                     desc: 'Our best picks for various needs and budgets.',
-                    img: '../public/images/image-top-laptops.jpg'
+                    img: './images/image-top-laptops.jpg'
             },
             {
                     id: 3,
                     number: 3,
                     title: 'The Growth of Gaming',
                     desc: 'How the pandemic has sparked fresh opportunities',
-                    img: '../public/images/image-gaming-growth.jpg'
+                    img: './images/image-gaming-growth.jpg'
             }
          
         ]
