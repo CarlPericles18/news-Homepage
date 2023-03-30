b91161394c9323312c14e232e8dd1c7a19423c2d
Perc <carlpericles18@gmail.com>
2023-03-31 00:59:16 +0900

    changed mobile-POV

--- newsPage/src/Nav.jsx
+++ newsPage/src/Nav.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Logo from "../public/images/logo.svg"
+import mobileLogo from "../public/images/icon-menu.svg"
+import mobileLogoClose from "../public/images/icon-menu-close.svg"
 
 
 export default function Nav(){
+    function close(){
+
+    }        
     return(
         <div className="navigation">
-            <img src={Logo} alt="LogoImg"></img>
-            <nav className="nav">
+            <img className ="desktopLogo"src={Logo} alt="LogoImg"></img>
+            <img  className="mobileLogo" src={mobileLogo} alt="LogoImg"></img>
+            <div className="nav">
                     <ul>Home</ul>
                     <ul>New</ul>
                     <ul>Popular</ul>
                     <ul>Trending</ul>
                     <ul>Categories</ul>
-            </nav>
+            </div>
+           
+            
         </div>
     )
 }
--- newsPage/src/Page.jsx
+++ newsPage/src/Page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Img from "../public/images/image-web-3-desktop.jpg"
+import ImgTwo from "../public/images/image-web-3-mobile.jpg"
 export default function Page(){
     return(
         <div className="page-Main">
             <img className="pageImg" src={Img} alt='PageImg'></img>
+            <img className="pageImg2" src={ImgTwo} alt='PageImg'></img>
             <div className="infoDiv">
                 <div><h1>THE BRIGHT Future of WEB 3.0?</h1></div>
                 <div className="para">
